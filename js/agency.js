88bfc3eb7c11d4f591ffd92c00763b62625a24b2
Glitch (aqa-work) <none>
2019-12-06 22:14:55 +0000

    👆🍍 Checkpoint ./css/agency.css:491517/168097 ./.glitch-assets:491517/807 ./index.html:491517/10 ./js/agency.js:491517/1803

--- js/agency.js
+++ js/agency.js
@@ -41,21 +41,78 @@
 
 })(jQuery); // End of use strict
 
-$(window).resize(function() {
-  var windowsize = $(window).width();
+// $(window).resize(function() {
+//   var windowsize = $(window).width();
 
-  if (windowsize <= 767) {
-    //if the window is greater than 440px wide then turn on jScrollPane..
-      // $(".res-break").css("display", "");
-      $(".services").removeClass("container");
-      // $(".service-left").removeClass("col");
-      $(".service-left").removeClass("text-right");
-      $(".service-left").addClass("text-left");
-  } else {
-    // $(".res-break").css("display", "none");
-    $(".services").addClass("container");
-      $(".service-left").removeClass("text-left");
-      $(".service-left").addClass("text-right");
-  }
+//   if (windowsize <= 767) {
+//     //if the window is greater than 440px wide then turn on jScrollPane..
+//       // $(".res-break").css("display", "");
+//       $(".services").removeClass("container");
+//       // $(".service-left").removeClass("col");
+//       $(".service-left").removeClass("text-right");
+//       $(".service-left").addClass("text-left");
+//   } else {
+//     // $(".res-break").css("display", "none");
+//     $(".services").addClass("container");
+//       $(".service-left").removeClass("text-left");
+//       $(".service-left").addClass("text-right");
+//   }
 
-});
+// });
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
